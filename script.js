bdcc40aad84b0b3b6b78f22a0652f87d01ddb777
dickersonj87 <dickersonj87@gmail.com>
2023-11-07 14:51:23 -0600

    button borders, glow for coins, results imgs

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ let playerCoins = 10;
 let wager = 1;
 let roundResult = '';
 
+const choices = ['rock', 'paper', 'scissors']; // Map numbers to choices
+
+function showSelections(playerSelection, computerSelection) {
+  const playerImage = document.getElementById('playerSelection');
+  const computerImage = document.getElementById('computerSelection');
+
+  playerImage.src = images[choices[playerSelection - 1]];
+  computerImage.src = images[choices[computerSelection - 1]];
+}
+
 // Updates score and checks if game is over
 function updateScore() {
   const scoreElement = document.getElementById('score');
@@ -46,7 +56,38 @@ function play(playerChoice) {
   }
 
   updateScore();
+  showSelections(playerChoice, computerChoice);
 }
+
+const wagerButtons = document.querySelectorAll('.coins'); // Replace '.coins' with the selector for your wager buttons
+
+wagerButtons.forEach(button => {
+  button.addEventListener('click', function() {
+    // Remove the selected class from all buttons
+    wagerButtons.forEach(btn => {
+      btn.classList.remove('selected');
+    });
+
+    // Add the selected class to the clicked button
+    this.classList.add('selected');
+  });
+});
+
+const images = {
+  1: 'images/original/ROCK.png', // Replace with your image paths
+  2: 'images/original/PAPER.png',
+  3: 'images/original/SCISSORS.png',
+};
+
+function showSelections(playerSelection, computerSelection) {
+  const playerImage = document.getElementById('playerSelection');
+  const computerImage = document.getElementById('computerSelection');
+
+  playerImage.src = images[playerSelection];
+  computerImage.src = images[computerSelection];
+}
+
+// Call this function in your game result function, passing the player's and computer's selections
 
 // Sets the wager amount
 function setWager(amount) {
